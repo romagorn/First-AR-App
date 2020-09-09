83098eb425f36441a98102d678694d4ea55789ea
romagorn <60058363+romagorn@users.noreply.github.com>
2020-09-09 15:14:48 +0600

    Initial Commit

---
+++ First-AR-App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  First-AR-App
+//
+//  Created by romangornostay on 9/9/20.
+//  Copyright © 2020 Roman Gornostayev. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
---
+++ First-AR-AppTests/First_AR_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  First_AR_AppTests.swift
+//  First-AR-AppTests
+//
+//  Created by romangornostay on 9/9/20.
+//  Copyright © 2020 Roman Gornostayev. All rights reserved.
+//
+
+import XCTest
+@testable import First_AR_App
+
+class First_AR_AppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
